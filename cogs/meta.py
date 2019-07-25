9085d93560581cbd23d7382893ba89cddcbabf97
Xua Yraili <williamblakelock.wb@gmail.com>
2019-07-26 10:28:59 +1200

    .

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,4 +1,9 @@
+import importlib
+import sys
+
 from discord.ext import commands
+
+from .utils.formats import format_exc
 
 
 class Meta(commands.Cog):
@@ -14,6 +19,16 @@ class Meta(commands.Cog):
 I am in very beta, be careful when using my commands as they are not ready for public use yet.
 Currently gazing over {len(ctx.bot.guilds)} servers, enjoying {len(ctx.bot.users):,} users' company.
 I don't have my own support server, so you can join my owners general server here: <https://discord.gg/hkweDCD>""")
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx, *, module):
+        try:
+            sys.modules[module] = importlib.reload(__import__(module))
+            await ctx.send(ctx.bot.tick_yes)
+        except Exception as e:
+            await ctx.send(ctx.bot.tick_no)
+            ctx.bot.send_error(f'```py\n{format_exc(e)}\n```')
 
 
 def setup(bot):
