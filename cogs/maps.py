d8f7dafe930e01f5f1980b518991aa8b66293304
Xua Yraili <williamblakelock.wb@gmail.com>
2019-08-10 14:44:58 +1200

    generate maps on boot

--- cogs/maps.py
+++ cogs/maps.py
@@ -5,7 +5,7 @@ from .utils.mapping import MapManager
 class Maps(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.mapmgr = MapManager.from_file("map.json")
+        self.mapmgr = MapManager.from_raw()
 
 
 def setup(bot):
