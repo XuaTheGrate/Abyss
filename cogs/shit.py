8a8d8fb174f99fccbdf9b7a76eee1b83aaacf49b
Xua Yraili <williamblakelock.wb@gmail.com>
2019-09-01 21:32:40 +1200

    bored

---
+++ cogs/shit.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+
+
+# ᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖᵠʳˢᵗᵘᵛʷˣʸᶻ
+smoltext = str.maketrans({'a': 'ᵃ', 'b': 'ᵇ', 'c': 'ᶜ', 'd': 'ᵈ', 'e': 'ᵉ', 'f': 'ᶠ', 'g': 'ᵍ', 'h': 'ʰ', 'i': 'ᶦ',
+                          'j': 'ʲ', 'k': 'ᵏ', 'l': 'ˡ', 'm': 'ᵐ', 'n': 'ⁿ', 'o': 'ᵒ', 'p': 'ᵖ', 'q': 'ᵠ', 'r': 'ʳ',
+                          's': 'ˢ', 't': 'ᵗ', 'u': 'ᵘ', 'v': 'ᵛ', 'w': 'ʷ', 'x': 'ˣ', 'y': 'ʸ', 'z': 'ᶻ'})
+
+
+class Shitpost(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['smoltext', 'smol', 'small'])
+    async def smalltext(self, ctx, *, text: commands.clean_content):
+        await ctx.send(text.translate(smoltext))
+
+
+def setup(bot):
+    bot.add_cog(Shitpost(bot))
